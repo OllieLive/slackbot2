0650c048f048d98d2f3a6a4f6b413ec7d8d5a34a
Itzik Fekete <myfekete@gmail.com>
2018-03-08 11:50:29 +0000

    saved

--- app.js
+++ app.js
@@ -14,15 +14,15 @@ app.listen(port, function () {
     console.log('Listening on port ' + port);
 });
 
-// app.post('/hello', function (req, res, next) {
-//     var userName = req.body.user_name;
-//     var botPayload = {
-//         text: 'Hello ' + userName + ', welcome to Devdactic Slack channel! I\'ll be your guide.'
-//     };
-//     // Loop otherwise..
-//     if (userName !== 'slackbot') {
-//         return res.status(200).json(botPayload);
-//     } else {
-//         return res.status(200).end();
-//     }
-// });
+app.post('/hello', function (req, res, next) {
+    var userName = req.body.user_name;
+    var botPayload = {
+        text: 'Hello ' + userName + ', welcome to Devdactic Slack channel! I\'ll be your guide.'
+    };
+    // Loop otherwise..
+    if (userName !== 'slackbot') {
+        return res.status(200).json(botPayload);
+    } else {
+        return res.status(200).end();
+    }
+});
